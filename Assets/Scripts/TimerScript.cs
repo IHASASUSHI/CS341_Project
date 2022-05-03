cec8b2c1bdbadeab7ee5bf29416f6e3c7ef0ac31
butlerdotexe <42647315+butlerdotexe@users.noreply.github.com>
2022-05-02 21:06:14 -0400

    Timer done
    Timer bar decreases and shows a game over text when empty

---
+++ Assets/Scripts/TimerScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour
+{
+    Image timerBar;
+    public float maxTime = 5f;
+    float timeLeft;
+    public GameObject gameoverText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameoverText.SetActive(false);
+        timerBar = GetComponent<Image>();
+        timeLeft = maxTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeLeft > 0)
+        {
+            timeLeft -= Time.deltaTime;
+            timerBar.fillAmount = timeLeft / maxTime;
+        }
+        else
+        {
+            gameoverText.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+}
